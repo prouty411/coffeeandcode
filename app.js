9e848e1ca13e0048dfdea26aae38ad2ca7c05607
Han-Mi Kim <tmpham1@uci.edu>
2018-10-02 18:04:54 -0700

    passing data from loading to final

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 $(document).ready(initializeApp);
 
 function initializeApp() {
-    $('#carousel').carousel()
+    $('#carousel').carousel();
+    $("#search").click(initiateSearch);
+    $('#library').click(selectType);
+    $('#cafe').click(selectType);
+  
 }
 function displayMap() {
-var mapProps = { 
+let mapProps = { 
     zoom: 8,
     center: {lat: -34.397, lng: 150.644}
 }
 var map = new google.maps.Map(document.getElementById("googleMap"), mapProps)
 }
+
+function selectType() {
+    if($(this).hasClass('highlight')) {
+        $(this).removeClass('highlight');
+        localStorage.clear();
+    }
+    else {
+    $(".type").removeClass('highlight')
+    $(this).addClass('highlight')
+    var type = $(this).attr('id')
+   console.log(type);
+   localStorage.setItem('type', `${type}`)
+    }
+   
+}
+
+function initiateSearch() {
+   let city  = $('#cityInput').val();
+   localStorage.setItem("city", `${city}`);
+    
+}
+
